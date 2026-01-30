a99f3edd35e71a138bc42b9512530d42c8e55168
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-30 18:01:50 +0100

    Add GameEvent

---
+++ client/event/GameEvent.cpp
@@ -0,0 +1,26 @@
+//
+// Created by chalo on 30.01.2026.
+//
+
+#include "GameEvent.h"
+
+GameEvent::GameEvent(const EventType type, const nlohmann::json &payload) {
+    this->type = type;
+    this->payload = payload;
+}
+
+EventType GameEvent::getType() const {
+    return type;
+}
+
+nlohmann::json GameEvent::getPayload() const {
+    return payload;
+}
+
+GameEvent parseEvent(std::string& message) {
+    nlohmann::json json = nlohmann::json::parse(message);
+    const auto type = json.value("type", "UNKNOWN");
+    const EventType eventType = stringToType.contains(type) ? stringToType[type] : EventType::UNKNOWN;
+    return {eventType, json};
+}
+
---
+++ client/event/GameEvent.h
@@ -0,0 +1,26 @@
+//
+// Created by chalo on 30.01.2026.
+//
+
+#ifndef GAMEEVENT_H
+#define GAMEEVENT_H
+
+#include <string>
+
+#include "EventType.h"
+#include <nlohmann/json.hpp>
+
+class GameEvent {
+    EventType type;
+        nlohmann::json payload;
+    public:
+    GameEvent(EventType type, const nlohmann::json &payload);
+    EventType getType() const;
+    nlohmann::json getPayload() const;
+};
+
+static GameEvent parseEvent(std::string& message);
+
+
+
+#endif //GAMEEVENT_H
