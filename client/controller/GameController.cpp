413b9edf16f9d57f38908d5c44ef50b4462c2270
Matysekxx <chaloupkamatej1353@gmail.com>
2026-01-30 18:01:37 +0100

    Add GameController

---
+++ client/controller/GameController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by chalo on 30.01.2026.
+//
+
+#include "GameController.h"
---
+++ client/controller/GameController.h
@@ -0,0 +1,16 @@
+//
+// Created by chalo on 30.01.2026.
+//
+
+#ifndef GAMECONTROLLER_H
+#define GAMECONTROLLER_H
+
+
+
+class GameController {
+
+};
+
+
+
+#endif //GAMECONTROLLER_H
